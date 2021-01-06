e0955da2724b2c733193c39263d830c1810717a9
Elwaze <eloise.lamontagne@gmail.com>
2021-01-06 11:24:08 +0100

    [1.0] git problem. Recreated git repository and committed what have been developped since now.

---
+++ maieuclic_project/permut_creation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PermutCreationConfig(AppConfig):
+    name = 'permut_creation'
---
+++ maieuclic_project/permut_management/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PermutManagementConfig(AppConfig):
+    name = 'permut_management'
