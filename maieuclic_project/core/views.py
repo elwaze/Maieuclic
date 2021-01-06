4d7dad72feeb413742498a9ad360c92ac69de419
Elwaze <eloise.lamontagne@gmail.com>
2021-01-06 13:46:45 +0100

    [1.1] empty functions in core.views to test travis settings

--- maieuclic_project/core/views.py
+++ maieuclic_project/core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# home
+def home(request):
+    pass
+    # return render(request, 'home.html')
+
+
+# legal notices
+def legal_notices(request):
+    pass
+    # return render(request, 'legal_notices.html')
+
+
+# legal notices
+def contact(request):
+    pass
+    # return render(request, 'contact.html')
