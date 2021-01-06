2d55b2f7d42c0477e2dd9d5cec7464991a62d2a1
Elwaze <eloise.lamontagne@gmail.com>
2021-01-06 14:28:14 +0100

    [1.1] updated urls to run tests

--- maieuclic_project/user/urls.py
+++ maieuclic_project/user/urls.py
@@ -1,9 +1,9 @@
-# from django.urls import re_path
-# from . import views
-#
-# urlpatterns = [
-#     re_path(r'^connection$', views.signin, name='signin'),
-#     re_path(r'^disconnection$', views.signout, name='signout'),
-#     re_path(r'^create_account$', views.signup, name='signup'),
-#     re_path(r'^my_account$', views.my_account, name='my_account')
-# ]
+from django.urls import re_path
+from . import views
+
+urlpatterns = [
+    # re_path(r'^connection$', views.signin, name='signin'),
+    # re_path(r'^disconnection$', views.signout, name='signout'),
+    # re_path(r'^create_account$', views.signup, name='signup'),
+    # re_path(r'^my_account$', views.my_account, name='my_account')
+]
