8f4949eb2725a75cc1a95100a347c91e153fd41f
Elwaze <eloise.lamontagne@gmail.com>
2021-01-27 14:53:14 +0100

    Update test_user.py

--- maieuclic_project/tests/integ/user/test_user.py
+++ maieuclic_project/tests/integ/user/test_user.py
@@ -67,7 +67,7 @@ class AccountTestCase(GeneralTestCase):
         # check the email content
         self.assertEqual(mail.outbox[0].subject, 'Finalisez la création de votre compte Maieuclic')
         self.assertEqual(mail.outbox[0].recipients()[0], 'created.{}'.format(EMAIL))
-        mail_body = mail.outbox[0].alternatives[0]
+        mail_body = mail.outbox[0].alternatives[0][0]
         print(mail_body)
         self.assertIn('Je confirme mon inscription', mail_body)
 
