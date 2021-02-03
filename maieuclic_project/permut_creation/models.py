b4a275e5487170d0907498571e43de83656183a6
Elwaze <eloise.lamontagne@gmail.com>
2021-02-03 10:55:13 +0100

    [4.1] initiated permut creation models

--- maieuclic_project/permut_creation/models.py
+++ maieuclic_project/permut_creation/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
+from user.models import MaieuclicUser
 
 # Create your models here.
+
+
+class PlaceManager(models.Manager):
+    pass
+
+
+class Place(models.Model):
+    city = models.CharField('Ville')
+    zipcode = models.CharField('Code Postal', max_length=5, validators=[MinLengthValidator(5)])
+    hospital_name = models.CharField('hôpital')
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+
+class PermutSearchManager(models.Manager):
+    def save_searched_place(self, place_id, email):
+        self.get_or_create(
+            place_id=place_id,
+            email=email
+        )
+
+
+class PermutSearch(models.Model):
+
+    class Meta:
+        unique_together = ('place_id', 'email')
+
+    objects = PermutSearchManager()
+    place_id = models.ForeignKey(Place, on_delete=models.CASCADE)
+    email = models.ForeignKey(MaieuclicUser, on_delete=models.CASCADE)
+
+
+
+
