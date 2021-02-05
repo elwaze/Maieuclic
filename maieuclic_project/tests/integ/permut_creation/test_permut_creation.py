579aa46d41fbe5924b3e30a1de4389f6c22ce43f
Elwaze <eloise.lamontagne@gmail.com>
2021-02-05 08:34:25 +0100

    [4.0] added checking returned result in permut creation integ tests

--- maieuclic_project/tests/integ/permut_creation/test_permut_creation.py
+++ maieuclic_project/tests/integ/permut_creation/test_permut_creation.py
@@ -14,7 +14,6 @@ class PermutCreationTestCase(GeneralTestCase):
         city = self.selenium.find_element_by_name("city")
         zipcode = self.selenium.find_element_by_name("zipcode")
         save_left_place = self.selenium.find_element_by_id("save_left_place")
-        save_searched_place = self.selenium.find_element_by_id("save_searched_place")
         # Fill the form with data
         city.send_keys('Voiron')
         zipcode.send_keys('38500')
@@ -22,18 +21,28 @@ class PermutCreationTestCase(GeneralTestCase):
         # submitting the form
         save_left_place.click()
         # check the returned result
+        self.assertIn('Recherche de permutation', self.selenium.page_source)
+        self.assertEqual(
+            self.selenium.current_url,
+            '{}/permut_creation/permut_search'.format(self.live_server_url),
+            "urlfound: " + self.selenium.current_url
+        )
 
     def test_searched_place_ok(self):
-        # a modifier
         # find the form element
         city = self.selenium.find_element_by_name("city")
         zipcode = self.selenium.find_element_by_name("zipcode")
-        save_left_place = self.selenium.find_element_by_id("save_left_place")
         save_searched_place = self.selenium.find_element_by_id("save_searched_place")
         # Fill the form with data
-        city.send_keys('Voiron')
-        zipcode.send_keys('38500')
+        city.send_keys('Grenoble')
+        zipcode.send_keys('38000')
 
         # submitting the form
-        save_left_place.click()
+        save_searched_place.click()
         # check the returned result
+        self.assertIn('Recherche de permutation', self.selenium.page_source)
+        self.assertEqual(
+            self.selenium.current_url,
+            '{}/permut_creation/permut_search'.format(self.live_server_url),
+            "urlfound: " + self.selenium.current_url
+        )
