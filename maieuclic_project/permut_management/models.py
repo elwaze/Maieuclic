262a412e79124a5a5ea49b95c26f9ec872d0896a
Elwaze <eloise.lamontagne@gmail.com>
2021-02-15 11:02:49 +0100

    [5.1] created models for permut management

--- maieuclic_project/permut_management/models.py
+++ maieuclic_project/permut_management/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+from user.models import MaieuclicUser
+from permut_creation.models import Place
+
 
 # Create your models here.
+
+
+class Permut(models.Model):
+    states = [('CR', 'created'), ('NO', 'notified'), ('BD', 'being discussed'), ('UN', 'under negociation'), ('RE', 'rejected')]
+    permut_id = models.AutoField(primary_key=True)
+    permut_state = models.CharField(choices=states, default='CR')
+    date_last_change = models.DateTimeField(auto_now=True)
+    users = ArrayField(models.EmailField())
+    date_time = models.DateTimeField(auto_now_add=True)
+
+
+class UserPermutAssoc(models.Model):
+    email_s = models.ForeignKey(MaieuclicUser, on_delete=models.CASCADE)
+    permut_id = models.ForeignKey(Permut, on_delete=models.CASCADE)
+    place_id = models.ForeignKey(Place, on_delete=models.CASCADE)
+    email = models.ForeignKey(MaieuclicUser, on_delete=models.CASCADE)
