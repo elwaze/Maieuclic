db28c25ed88505f518a127b81d6b254e437b44bc
Elwaze <eloise.lamontagne@gmail.com>
2021-02-15 14:10:31 +0100

    [5.1] initiated permut management view (empty)

--- maieuclic_project/permut_management/views.py
+++ maieuclic_project/permut_management/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 
 def my_permut(request):
+    # recuperer les permuts assoc a l'utilisateur
+    # afficher la liste des permuts : poste a recuperer, etat de la permut, coord de la personne qui donne ce poste si auth ok, coord de la personne a qui l'user donne son poste si auth ok
+    # possibilite de changer l'etat de la permut.
+    # si etat passe a BD ou UN, passer tous les user_state a false.
+    # si etat permut passe a RE, repasser tous les user_state a true
     pass
