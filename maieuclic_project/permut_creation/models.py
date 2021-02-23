fdafdc77355f3b6ac335541e768bd73f90213320
Elwaze <eloise.lamontagne@gmail.com>
2021-02-23 11:31:24 +0100

    [4.1] permit creation saves cities uppercase. todo : multi search

--- maieuclic_project/permut_creation/models.py
+++ maieuclic_project/permut_creation/models.py
@@ -8,8 +8,8 @@ from django.core.validators import MinLengthValidator
 
 class PlaceManager(models.Manager):
     def create_place(self, city, zipcode):
-        self.get_or_create(
-            city=city,
+        return self.get_or_create(
+            city=city.upper(),
             zipcode=zipcode,
         )
 
@@ -24,13 +24,12 @@ class Place(models.Model):
     place_id = models.AutoField(primary_key=True)
     city = models.CharField('Ville', max_length=50)
     zipcode = models.CharField('Code Postal', max_length=5, validators=[MinLengthValidator(5)])
-    lat = models.DecimalField(max_digits=19, decimal_places=16, null=True, blank=True)
-    lng = models.DecimalField(max_digits=19, decimal_places=16, null=True, blank=True)
+    # lat = models.DecimalField(max_digits=19, decimal_places=16, null=True, blank=True)
+    # lng = models.DecimalField(max_digits=19, decimal_places=16, null=True, blank=True)
 
 
 class PermutSearchManager(models.Manager):
     def save_searched_place(self, place_id, email):
-        # si pas de place_id, creer une place ?
         self.get_or_create(
             place_id=place_id,
             email=email
