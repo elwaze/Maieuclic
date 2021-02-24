5f4a548decec8888f31c7ac0d71ab99f299ff4e1
Elwaze <eloise.lamontagne@gmail.com>
2021-02-24 11:09:38 +0100

    [4.1] separate leave and serch forms actions

--- maieuclic_project/permut_creation/urls.py
+++ maieuclic_project/permut_creation/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('permut_search', views.permut_creation, name='permut_search'),
+    # path('permut_search', views.permut_creation, name='permut_search'),
+    path('search_place', views.search_place, name='search_place'),
+    path('leave_place', views.leave_place, name='leave_place'),
 ]
