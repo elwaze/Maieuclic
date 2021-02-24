609f02a9226fb8c1b428212b4424026108a3ead9
Elwaze <eloise.lamontagne@gmail.com>
2021-02-24 12:09:32 +0100

    [4.1] added an empty form in places searched to add a place searched

--- maieuclic_project/permut_creation/views.py
+++ maieuclic_project/permut_creation/views.py
@@ -7,7 +7,6 @@ from user.models import MaieuclicUser
 
 
 def permut_creation(request):
-    print(request)
     error = False
     if request.method == "POST":
         search_form = PlaceForm(request.POST, prefix="search")
@@ -15,9 +14,6 @@ def permut_creation(request):
             city = search_form.cleaned_data['city']
             zipcode = search_form.cleaned_data['zipcode']
             place = Place.objects.create_place(city, zipcode)[0]
-            print('place searched')
-            print(place)
-            print(place.place_id, place.city, place.zipcode)
             place_searched = PermutSearch.objects.save_searched_place(place, request.user)
 
             return render(request, 'permut_search.html', locals())
@@ -27,15 +23,8 @@ def permut_creation(request):
         leave_form = PlaceForm(request.POST, prefix="leave")
         if leave_form.is_valid():
             city = leave_form.cleaned_data['city']
-            print(city)
             zipcode = leave_form.cleaned_data['zipcode']
-            print(zipcode)
             place_left = Place.objects.create_place(city, zipcode)[0]
-            print("place left")
-            print(place_left)
-            print(place_left.city)
-            print(place_left.zipcode)
-            print(place_left.place_id)
             MaieuclicUser.objects.save_place_left(request.user.email, place_left)
             return render(request, 'permut_search.html', locals())
 
@@ -45,23 +34,15 @@ def permut_creation(request):
     else:
         search_form = PlaceForm(prefix="search")
         try:
-            # boucle for pour tous les places + 1
-            # ajouter id dans le form avec id de la recherche en DB
-            # cf formset
             place_searched = PermutSearch.objects.filter(email=request.user)[0].place_id
-            print('place_searched')
-            print(place_searched)
             search_form.fields['city'].initial = place_searched.city
             search_form.fields['zipcode'].initial = place_searched.zipcode
         except IndexError:
             pass
-        # place_searched = Place.objects.get(pk=place_searched.place_id)
 
         leave_form = PlaceForm(prefix="leave")
         try:
             place_left = Place.objects.get(pk=request.user.place_id.place_id)
-            print('place_left')
-            print(place_left.city)
             leave_form.fields['city'].initial = place_left.city
             leave_form.fields['zipcode'].initial = place_left.zipcode
         except AttributeError:
@@ -76,15 +57,8 @@ def leave_place(request):
         leave_form = PlaceForm(request.POST, prefix="leave")
         if leave_form.is_valid():
             city = leave_form.cleaned_data['city']
-            print(city)
             zipcode = leave_form.cleaned_data['zipcode']
-            print(zipcode)
             place_left = Place.objects.create_place(city, zipcode)[0]
-            print("place left")
-            print(place_left)
-            print(place_left.city)
-            print(place_left.zipcode)
-            print(place_left.place_id)
             MaieuclicUser.objects.save_place_left(request.user.email, place_left)
             search_forms = []
             places_searched = PermutSearch.objects.filter(email=request.user)
@@ -95,18 +69,8 @@ def leave_place(request):
                 search_form.fields['city'].initial = place_searched.city
                 search_form.fields['zipcode'].initial = place_searched.zipcode
                 search_forms.append(search_form)
-            # search_form = PlaceForm(prefix="search")
-            # try:
-            #     # boucle for pour tous les places + 1
-            #     # ajouter id dans le form avec id de la recherche en DB
-            #     # cf formset
-            #     place_searched = PermutSearch.objects.filter(email=request.user)[0].place_id
-            #     print('place_searched')
-            #     print(place_searched)
-            #     search_form.fields['city'].initial = place_searched.city
-            #     search_form.fields['zipcode'].initial = place_searched.zipcode
-            # except IndexError:
-            #     pass
+            search_form = PlaceForm(prefix="search")
+            search_forms.append(search_form)
 
             return render(request, 'permut_search.html', locals())
 
@@ -123,83 +87,16 @@ def leave_place(request):
             search_form.fields['city'].initial = place_searched.city
             search_form.fields['zipcode'].initial = place_searched.zipcode
             search_forms.append(search_form)
-        # search_form = PlaceForm(prefix="search")
-        # try:
-        #     # boucle for pour tous les places + 1
-        #     # ajouter id dans le form avec id de la recherche en DB
-        #     # cf formset
-        #     place_searched = PermutSearch.objects.filter(email=request.user)[0].place_id
-        #     print('place_searched')
-        #     print(place_searched)
-        #     search_form.fields['city'].initial = place_searched.city
-        #     search_form.fields['zipcode'].initial = place_searched.zipcode
-        # except IndexError:
-        #     pass
-        # place_searched = Place.objects.get(pk=place_searched.place_id)
-
+        search_form = PlaceForm(prefix="search")
+        search_forms.append(search_form)
         leave_form = PlaceForm(prefix="leave")
         try:
             place_left = Place.objects.get(pk=request.user.place_id.place_id)
-            print('place_left')
-            print(place_left.city)
             leave_form.fields['city'].initial = place_left.city
             leave_form.fields['zipcode'].initial = place_left.zipcode
         except AttributeError:
             pass
     return render(request, 'permut_search.html', locals())
-
-#
-# def search_place(request):
-#     error = False
-#     if request.method == "POST":
-#         search_form = PlaceForm(request.POST, prefix="search")
-#         if search_form.is_valid():
-#             city = search_form.cleaned_data['city']
-#             zipcode = search_form.cleaned_data['zipcode']
-#             place = Place.objects.create_place(city, zipcode)[0]
-#             print('place searched')
-#             print(place)
-#             print(place.place_id, place.city, place.zipcode)
-#             place_searched = PermutSearch.objects.save_searched_place(place, request.user)
-#             leave_form = PlaceForm(prefix="leave")
-#             try:
-#                 place_left = Place.objects.get(pk=request.user.place_id.place_id)
-#                 print('place_left')
-#                 print(place_left.city)
-#                 leave_form.fields['city'].initial = place_left.city
-#                 leave_form.fields['zipcode'].initial = place_left.zipcode
-#             except AttributeError:
-#                 pass
-#
-#             return render(request, 'permut_search.html', locals())
-#         else:
-#             error = True
-#
-#     else:
-#         search_form = PlaceForm(prefix="search")
-#         try:
-#             # boucle for pour tous les places + 1
-#             # ajouter id dans le form avec id de la recherche en DB
-#             # cf formset
-#             place_searched = PermutSearch.objects.filter(email=request.user)[0].place_id
-#             print('place_searched')
-#             print(place_searched)
-#             search_form.fields['city'].initial = place_searched.city
-#             search_form.fields['zipcode'].initial = place_searched.zipcode
-#         except IndexError:
-#             pass
-#         # place_searched = Place.objects.get(pk=place_searched.place_id)
-#
-#         leave_form = PlaceForm(prefix="leave")
-#         try:
-#             place_left = Place.objects.get(pk=request.user.place_id.place_id)
-#             print('place_left')
-#             print(place_left.city)
-#             leave_form.fields['city'].initial = place_left.city
-#             leave_form.fields['zipcode'].initial = place_left.zipcode
-#         except AttributeError:
-#             pass
-#     return render(request, 'permut_search.html', locals())
 
 
 def search_place(request):
@@ -210,7 +107,6 @@ def search_place(request):
         for permut in places_searched:
             place_searched = permut.place_id
             search_form = PlaceForm(prefix="search")
-
             search_form.fields['city'].initial = place_searched.city
             search_form.fields['zipcode'].initial = place_searched.zipcode
             search_forms.append(search_form)
@@ -219,16 +115,13 @@ def search_place(request):
             city = search_form.cleaned_data['city']
             zipcode = search_form.cleaned_data['zipcode']
             place = Place.objects.create_place(city, zipcode)[0]
-            print('place searched')
-            print(place)
-            print(place.place_id, place.city, place.zipcode)
             place_searched = PermutSearch.objects.save_searched_place(place, request.user)
-            # search_forms.append(search_form)
+            search_forms.append(search_form)
+            search_form = PlaceForm(prefix="search")
+            search_forms.append(search_form)
             leave_form = PlaceForm(prefix="leave")
             try:
                 place_left = Place.objects.get(pk=request.user.place_id.place_id)
-                print('place_left')
-                print(place_left.city)
                 leave_form.fields['city'].initial = place_left.city
                 leave_form.fields['zipcode'].initial = place_left.zipcode
             except AttributeError:
@@ -248,26 +141,11 @@ def search_place(request):
             search_form.fields['city'].initial = place_searched.city
             search_form.fields['zipcode'].initial = place_searched.zipcode
             search_forms.append(search_form)
-
-        # search_form = PlaceForm(prefix="search")
-        # try:
-        #     # boucle for pour tous les places + 1
-        #     # ajouter id dans le form avec id de la recherche en DB
-        #     # cf formset
-        #     place_searched = PermutSearch.objects.filter(email=request.user)[0].place_id
-        #     print('place_searched')
-        #     print(place_searched)
-        #     search_form.fields['city'].initial = place_searched.city
-        #     search_form.fields['zipcode'].initial = place_searched.zipcode
-        # except IndexError:
-        #     pass
-        # place_searched = Place.objects.get(pk=place_searched.place_id)
-
+        search_form = PlaceForm(prefix="search")
+        search_forms.append(search_form)
         leave_form = PlaceForm(prefix="leave")
         try:
             place_left = Place.objects.get(pk=request.user.place_id.place_id)
-            print('place_left')
-            print(place_left.city)
             leave_form.fields['city'].initial = place_left.city
             leave_form.fields['zipcode'].initial = place_left.zipcode
         except AttributeError:
