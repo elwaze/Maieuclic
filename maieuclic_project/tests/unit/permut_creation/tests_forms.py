d2badfeea7fbe86a88d79c48725fe6f858365dfc
Elwaze <eloise.lamontagne@gmail.com>
2021-02-26 11:24:30 +0100

    [2.1] added Sian website link in legals [4.0] removed unused code in tests forms

--- maieuclic_project/tests/unit/permut_creation/tests_forms.py
+++ maieuclic_project/tests/unit/permut_creation/tests_forms.py
@@ -1,37 +1,12 @@
-from django.core.exceptions import ValidationError
-from django.test import TestCase, Client
+from django.test import TestCase
 
 from permut_creation.forms import PlaceForm
-from permut_creation.models import Place, PermutSearch
-from user.models import MaieuclicUser
 
 
 class TestPlaceForms(TestCase):
     """
     permut_creation app forms test.
     """
-    #
-    # def setUp(self):
-    #     self.email = 'moi@gmail.com'
-    #     self.password = 'moi'
-    #     self.client = Client()
-    #     self.user = MaieuclicUser.objects.create_user(email=self.email, password=self.password)
-    #     self.left_place = {
-    #         'city': 'Voiron',
-    #         'zipcode': '38500'
-    #     }
-    #     Place.objects.create(
-    #         city=self.left_place['city'],
-    #         zipcode=self.left_place['zipcode']
-    #     )
-    #     self.searched_place = {
-    #         'city': 'La Tronche',
-    #         'zipcode': '38700',
-    #     }
-    #     Place.objects.create(
-    #         city=self.searched_place['city'],
-    #         zipcode=self.searched_place['zipcode'],
-    #     )
 
     def test_place_form_ok(self):
         """
