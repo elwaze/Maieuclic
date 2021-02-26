777113b33c589dece0bb0aeb26e41f7c6db895ad
Elwaze <eloise.lamontagne@gmail.com>
2021-02-26 11:18:44 +0100

    [4.0] test forms commented unused code, and added tests with wrong values in the form

--- maieuclic_project/tests/unit/permut_creation/tests_forms.py
+++ maieuclic_project/tests/unit/permut_creation/tests_forms.py
@@ -10,47 +10,52 @@ class TestPlaceForms(TestCase):
     """
     permut_creation app forms test.
     """
+    #
+    # def setUp(self):
+    #     self.email = 'moi@gmail.com'
+    #     self.password = 'moi'
+    #     self.client = Client()
+    #     self.user = MaieuclicUser.objects.create_user(email=self.email, password=self.password)
+    #     self.left_place = {
+    #         'city': 'Voiron',
+    #         'zipcode': '38500'
+    #     }
+    #     Place.objects.create(
+    #         city=self.left_place['city'],
+    #         zipcode=self.left_place['zipcode']
+    #     )
+    #     self.searched_place = {
+    #         'city': 'La Tronche',
+    #         'zipcode': '38700',
+    #     }
+    #     Place.objects.create(
+    #         city=self.searched_place['city'],
+    #         zipcode=self.searched_place['zipcode'],
+    #     )
 
-    def setUp(self):
-        self.email = 'moi@gmail.com'
-        self.password = 'moi'
-        self.client = Client()
-        self.user = MaieuclicUser.objects.create_user(email=self.email, password=self.password)
-        self.left_place = {
-            'city': 'Voiron',
-            'zipcode': '38500'
-        }
-        Place.objects.create(
-            city=self.left_place['city'],
-            zipcode=self.left_place['zipcode']
-        )
-        self.searched_place = {
-            'city': 'La Tronche',
-            'zipcode': '38700',
-        }
-        Place.objects.create(
-            city=self.searched_place['city'],
-            zipcode=self.searched_place['zipcode'],
-        )
-
-    def test_place_form(self):
+    def test_place_form_ok(self):
         """
         Tests if the user tries to create place left or searched.
         """
         data = {
-            'city': self.left_place['city'],
-            'zipcode': self.left_place['zipcode']
+            'city': 'La Tronche',
+            'zipcode': '38700'
         }
         form = PlaceForm(data=data)
         self.assertTrue(form.is_valid())
 
-    # def test_search_place_form(self):
-    #     """
-    #     Tests if the user tries to create searched place.
-    #     """
-    #     data = {
-    #         'city': self.searched_place['city'],
-    #         'zipcode': self.searched_place['zipcode']
-    #     }
-    #     form = PlaceForm(data=data)
-    #     self.assertTrue(form.is_valid())
+    def test_place_form_wrong_zipcode(self):
+        data = {
+            'city': 'La Tronche',
+            'zipcode': '387001'
+        }
+        form = PlaceForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_place_form_wrong_city(self):
+        data = {
+            'city': 38700,
+            'zipcode': '38700'
+        }
+        form = PlaceForm(data=data)
+        self.assertFalse(form.is_valid())
